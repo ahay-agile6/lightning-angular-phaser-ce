b35f4479e13a208c2a01a7b3c787d63b05c69be1
Aurora Hay <aurora.hay@agile6.com>
2018-04-23 16:51:15 -0700

    example of how to use phaser-ce in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GameComponent } from './game/game.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/',
+    pathMatch: 'full'
+  },
+  {
+    path: 'game',
+    component: GameComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
